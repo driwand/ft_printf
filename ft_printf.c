a5c7b07d41e370f924f384ab174a8bd22612f767
Abdelhakim Kssiba <abkssiba@e1r10p4.1337.ma>
2019-11-27 20:53:30 +0100

    s is working well, without flags

--- ft_printf.c
+++ ft_printf.c
@@ -14,117 +14,61 @@
 #include "libft/libft.h"
 #include <stdarg.h>
 #include <stdlib.h>
-#include "libft/libft.h"
 
-int    ft_get_arr(const char *str, char c)
+void  ft_putstr(char *str)
 {
-	int             count;
-	int             i;
-	int             j;
-
+	int i;
 	i = 0;
-	count = 0;
-	while (str[i])
+	while(str[i] != '\0')
 	{
-		j = i;
-		while (str[j] != c && str[j] != '\0')
-			j++;
-		if (j > i)
-		{
-			count++;
-			i = j;
-			continue ;
-		}
+		putchar(str[i]);
 		i++;
 	}
-	return (count);
 }
 
-char *new_str(char *old, int index, char *str, int len)
+void  ft_get_rest(char *str, int start, int end)
 {
-	char *new;
 	int i;
-	int j;
-	int k;
 
-	k = 0;
-	j = 0;
-	new = malloc(len);
 	i = 0;
-
-	while (index--)
+	while(str[i] != '\0')
 	{
-		new[i] = old[i];
+		putchar(str[i]);
 		i++;
 	}
-	printf("%d %d %d %s | %s | %s\n",len, i, index, old, new, str);
-	while (i < len)
-	{
-		new[i] = str[j];
-		i++;
-		j++;
-	}
-	printf("%c %d %d %c %d\n", new[len - 1], len , j, old[i], i);
-	k = 3;
-	while (j < len && k > 0)
-	{
-		new[len - k] = old[j];
-		k--;
-		j++;
-	}
-	printf("%d %d %d %s | %s | %s\n",len, i, index, old, new, str);
-	new[len] = '\0';
-	return (new);
 }
 
-int ft_printf(const char *s,...)
-{
-	int count;
-	int i;
-	char *str;
-	va_list arguments;
-	int len;
-	char *tmp;
-	int len_s;
-	char *new;
 
-	i = 0;
-	if (!s)
-		return (-1);
-	len = 0;
-	len_s = ft_strlen(s);
-	count = ft_get_arr(s, '%');
-	if (count == 0)
-		ft_putstr_fd((char *)s, 1);
-	else
-	{
-		va_start(arguments, s);
-		while (s[i])
-		{
-			if (s[i] == '%')
-			{
-				if (s[i + 1] == 's')
-					str = va_arg(arguments, char*);
-				len += ft_strlen(str);
-				tmp = malloc(len + len_s + 1);
-				if (!new)
-				{
-					new = new_str((char*)s, i, str, len + len_s + 1 - 2);
-					i++;
-					//printf("%s\n", new);
-					continue ;
-				}
-				new = new_str(new, i, str, len + len_s + 1);
-			}
-			i++;	
-		}
-	}
-	va_end(arguments);
-	return (0);
+int ft_printf(const char * str, ...)
+{
+    va_list arg;
+    int i;
+
+    i = 0;
+    va_start(arg, str);
+    while (str[i])
+    {
+        while (str[i] == '%')
+        {
+            if (str[i + 1] == 's')
+                ft_putstr_fd(va_arg(arg, char*), 1);
+            else if (str[i + 1] == '%')
+                ft_putchar_fd('%', 1);
+            if (str[i + 1] == '%' || str[i + 1] == 's') {
+                i = i + 2;
+                continue;
+            }
+            i++;
+        }
+        if (str[i])
+            ft_putchar_fd(str[i], 1);
+        if(str[i])
+            i++;
+    }
+    return (0);
 }
 
 int main()
 {
-	ft_printf("hut %s az", "wxc");
-    return (0);
+	ft_printf("%lo");
 }
